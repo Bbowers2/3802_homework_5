0c9f3dd6a2fa3655e21acd358a5cd996b1073e52
Brian Bowers <bmbowers22@gmail.com>
2025-05-07 17:46:13 -0700

    fix

--- src/regex_exercises.js
+++ src/regex_exercises.js
@@ -5,7 +5,7 @@ const regexes = {
   masterCard:
     /^((5[1-5]\d{14})|(222[1-9]\d{12})|(22[3-9]\d{13})|(2[3-7]\d{14})|(27[01]\d{13})|(2720\d{12}))$/,
   notThreeEndingInOO: /^([A-Za-z](?![oO][oO]$)[A-Za-z]*)?$/iu,
-  divisibleBy16: /^(([01]*0000)|000|00|0)$/,
+  divisibleBy16: /^(([01]*0000)|0{1,3})$/,
   eightThroughThirtyTwo: /^((8|9)|([12][0-9)])|(3[012]))$/,
   notPythonPycharmPyc: /^(?!python$)(?!pycharm$)(?!pyc$)\p{L}*$/u,
   restrictedFloats: /^\d*(.\d+)?[eE][+-]?\d{1,3}$/i,
